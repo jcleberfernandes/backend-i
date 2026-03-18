7f5be8f64e83f2cc1d37fd43fafea737fb5d1a0e
Cleber Fernandes <193790081+Corujitoo@users.noreply.github.com>
2026-03-18 18:23:29 +0000

    Update IndexView and DetailView logic in views.py

--- django_project/polls/views.py
+++ django_project/polls/views.py
@@ -11,14 +11,14 @@ class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
-def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
-        :5
-    ]
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]
 
 
 class DetailView(generic.DetailView):
@@ -34,6 +34,7 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -55,9 +56,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-
-
-
-
-
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
